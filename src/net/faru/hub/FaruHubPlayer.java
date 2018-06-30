33745a75611877991a73e0806a7fe8e4674a79f6
Antoine Gaming <Antoine Gaming@192.168.1.31>
2018-06-30 15:04:19 +0200

    Update 0.0.5 by ChocoIG (Scoreboard setup)

--- src/net/faru/hub/FaruHubPlayer.java
+++ src/net/faru/hub/FaruHubPlayer.java
@@ -6,6 +6,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import net.faru.api.spigot.player.FaruPlayer;
+import net.faru.api.spigot.player.currency.Currency;
 import net.faru.api.tools.board.ScoreboardSign;
 import net.faru.api.tools.board.TeamsTagsManager;
 
@@ -35,16 +36,17 @@ public class FaruHubPlayer {
 		this.scoreboard.create();
 		this.scoreboard.setObjectiveName("§c§lF§e§la§a§lr§b§lu§d§lGames");
 		
-		this.scoreboard.setLine(10, "§a");
-		this.scoreboard.setLine(9, "§b");
-		this.scoreboard.setLine(8, "§c");
-		this.scoreboard.setLine(7, "§d");
-		this.scoreboard.setLine(6, "§e");
-		this.scoreboard.setLine(5, "§f");
-		this.scoreboard.setLine(4, "§1");
+		this.scoreboard.setLine(0, "§1");
+		this.scoreboard.setLine(1, "§7Pseudo: " + "§a" + faruPlayer.getPlayer().getName());
+		this.scoreboard.setLine(2, "§7Rank: " + faruPlayer.getRank().getColor() + faruPlayer.getRank().getPrefix());
 		this.scoreboard.setLine(3, "§2");
-		this.scoreboard.setLine(2, "§3");
-		this.scoreboard.setLine(1, "§4");
-		this.scoreboard.setLine(0, "§5");
+		this.scoreboard.setLine(4, "§7Coins: §e" + faruPlayer.getCoins(Currency.COINS));
+		this.scoreboard.setLine(5, "§7Credits: §d" + faruPlayer.getCoins(Currency.CREDITS));
+		this.scoreboard.setLine(6, "§3");
+		this.scoreboard.setLine(7, "§7Hub: §3" + Bukkit.getServerName());
+		this.scoreboard.setLine(8, "§7Players: §b" + "int");
+		this.scoreboard.setLine(9, "§4");
+		this.scoreboard.setLine(10, "§eplay.farugames.net");
+		this.scoreboard.setLine(11, "§8§m+---------------+");
 	}
 }
