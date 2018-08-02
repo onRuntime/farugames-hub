66cdac52e75273e177837ae1c7f0b45c8bec1f5b
3DX900 <3DX900@DESKTOP-8ADURG8.home>
2018-08-02 19:33:59 +0200

    Update

--- src/net/farugames/hub/utils/Item.java
+++ src/net/farugames/hub/utils/Item.java
@@ -6,10 +6,10 @@ import net.farugames.api.tools.builders.items.ItemBuilder.ItemType;
 
 public enum Item {
 
-	MENU_GAME("hub", null, 0, new ItemBuilder(ItemType.CUSTOM_HEAD, "http://textures.minecraft.net/texture/a6bc364673c34dc34a9578292f30aa9f2d4b9468115b86129e76786cff8299d").withName("§cGame Menu")),
+	MENU_GAME("hub", null, 0, new ItemBuilder(ItemType.CUSTOM_HEAD, "http://textures.minecraft.net/texture/a6bc364673c34dc34a9578292f30aa9f2d4b9468115b86129e76786cff8299d").withName("Â§CGame Menu")),
 	MENU_PROFILE("hub", null, 1, null),
-	MENU_COSMETICS_OPEN("hub", null, 4, new ItemBuilder(ItemType.CUSTOM_HEAD, "http://textures.minecraft.net/texture/b4cc193f6edd88a772b93eea3abd7520ce945d73864a62fec1ff5c2f94e25").withName("§bCosmetics")),
-	MENU_COSMETICS_CLOSE("hub", null, 4, new ItemBuilder(ItemType.CUSTOM_HEAD, "http://textures.minecraft.net/texture/b4fcaaea16eb7271067d8c8b3274de0ce762fa14f886a829dacf0457630eab").withName("§bCosmetics"));
+	MENU_COSMETICS_OPEN("hub", null, 4, new ItemBuilder(ItemType.CUSTOM_HEAD, "http://textures.minecraft.net/texture/b4cc193f6edd88a772b93eea3abd7520ce945d73864a62fec1ff5c2f94e25").withName("Â§bCosmectics")),
+	MENU_COSMETICS_CLOSE("hub", null, 4, new ItemBuilder(ItemType.CUSTOM_HEAD, "http://textures.minecraft.net/texture/b4fcaaea16eb7271067d8c8b3274de0ce762fa14f886a829dacf0457630eab").withName("Â§bCosmectics"));
 	
 	private String type;
 	private Rank accessRank;
